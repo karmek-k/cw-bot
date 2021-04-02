ad88bf8e5ed4ff52b702e4d117ae76e6c593d800
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-04-03 00:04:12 +0200

    Use CommandHandler to handle commands

--- index.php
+++ index.php
@@ -6,9 +6,16 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use CWBot\Bot;
+use CWBot\Service\CommandHandler;
+use CWBot\Service\CommandLoader;
 
 // initialize env. vars
 Dotenv::createImmutable(__DIR__)->load();
 
+$loader = new CommandLoader('config/commands');
+$commands = $loader->getCommands();
+
+$commandHandler = new CommandHandler($commands, $_ENV['PREFIX']);
+
 $bot = new Bot($_ENV['BOT_TOKEN']);
-$bot->run(function ($discord) { $discord->close(); });
+$bot->run($commandHandler->handleCommands);
