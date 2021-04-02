cd8c387198fab2badacb3d38b573fe7acfe5f9ae
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-04-02 23:32:17 +0200

    Create the Ping command

--- src/Command/BaseCommand.php
+++ src/Command/BaseCommand.php
@@ -5,7 +5,18 @@ namespace CWBot\Command;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 
+/**
+ * The base class that all command classes
+ * extend.
+ */
 abstract class BaseCommand
 {
+    public function __construct(private ?string $help = null) {}
+
+    public function getHelp(): ?string
+    {
+        return $this->help;
+    }
+
     abstract public function action(Message $message, Discord $discord);
 }
--- src/Command/Ping.php
+++ src/Command/Ping.php
@@ -5,8 +5,11 @@ namespace CWBot\Command;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 
-class Ping extends BaseCommand
+class Ping implements BaseCommand
 {
+    /**
+     * Replies `Pong!` to the user.
+     */
     public function action(Message $message, Discord $discord)
     {
         $message->reply('Pong!');
